37eeb77c02ec4864f452e6e1fc63c7d9e485380a
K M Marziul Karim <marziulrafi@gmail.com>
2025-03-13 12:29:56 +0600

    thumbnail dynamic

--- index.js
+++ index.js
@@ -1,22 +1,56 @@
-function loadCategories () {
-    fetch(" https://openapi.programming-hero.com/api/phero-tube/categories")
-    .then((res)=>res.json())
-    .then((data)=>displayCategories(data.categories))
+function loadCategories() {
+    fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+        .then((res) => res.json())
+        .then((data) => displayCategories(data.categories))
 }
-
+function loadVideos() {
+    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+        .then((response) => response.json())
+        .then((data) => displayVideos(data.videos))
+}
 function displayCategories(categories) {
-   const categoryContainer = document.getElementById("category");
+    const categoryContainer = document.getElementById("category");
 
-   for (let cate of categories) {
-    console.log(cate);
+    for (let cate of categories) {
+        console.log(cate);
 
-    const categoryDiv = document.createElement("div");
-    categoryDiv.innerHTML =
-    `
+        const categoryDiv = document.createElement("div");
+        categoryDiv.innerHTML =
+            `
     <button class = "btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cate.category}</button>
     `;
 
-    categoryContainer.append(categoryDiv)
-   }
+        categoryContainer.append(categoryDiv)
+    }
 }
+
+const displayVideos = (videos) => {
+    const videoContainer = document.getElementById("video-container");
+
+    videos.forEach((video) => {
+        console.log(video);
+        const videoCard = document.createElement("div");
+
+        videoCard.innerHTML = `
+        <div class="card bg-base-100 shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+        `;
+
+        videoContainer.append(videoCard)
+    })
+};
+
 loadCategories();
+loadVideos();
